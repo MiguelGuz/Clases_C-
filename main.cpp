8bd893fd8e6abe51db651d050625b6d5b0916153
MiguelGuz <jose.guzman1343@alumnos.udg.mx>
2020-10-19 15:36:52 -0500

    cambio de sitacion de las funciones para el uso de las sobrecargas a la hora de ejecutar el programa

--- main.cpp
+++ main.cpp
@@ -6,19 +6,27 @@ using namespace std;
 
 int main(){
 
-    computadora c02 = computadora("pc personal",
+    computadora c02 = computadora(  "migse",
                                     "windows",
                                     "lenovo",
                                     4.00);
     computadora c03;
-    c03.setNombre("pc personal");
+    c03.setNombre("usuario");
     c03.setSistemaop("windows");
-    c03.setMarca("Hp");
+    c03.setMarca("microsoft");
     c03.setMemoriaRam(8.00);
 
     laboratorio lb;
-    lb.agregarcomputadora(c02);
-    lb.agregarcomputadora(c03);
+
+    //lb.agregarcomputadora(c02);
+    //lb.agregarcomputadora(c03);
+
+    lb<<c02<<c03;
+
+    //cout<<c02;
+    computadora c04;
+    cin >> c04;
+    lb << c04;
 
     lb.mostrar();
 
