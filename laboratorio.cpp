103dc25c07a109e89021f4ed8a9ad2d339ae6b13
MiguelGuz <jose.guzman1343@alumnos.udg.mx>
2020-10-19 15:35:05 -0500

    referencia a archivos de computadora.h y agregacion de linea que sirve como encabezado de las columnas

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -19,12 +19,19 @@ void laboratorio::agregarcomputadora(const computadora &p)
 
 void laboratorio::mostrar()
 {
+    cout << left;
+    cout <<setw(13)<<"nombre.";
+    cout <<setw(12)<<"sistemaop.";
+    cout <<setw(10)<<"PC marca.";
+    cout <<setw(10)<<"MemoriaRam.";
+    cout <<endl;
     for(size_t i=0;i<cont;i++){
         computadora &c=arreglo[i];
-        cout <<"Nombre del Equipo: "<<arreglo[i].getNombre()<<endl;
-        cout <<"Sistema Operativo: "<<arreglo[i].getSistemaop()<<endl;
-        cout <<"Marca del Equipo: "<<arreglo[i].getMarca()<<endl;
-        cout <<"Memoria Ram: "<<arreglo[i].getMemoriaRam()<<"gb"<<endl;
-        cout<<"-------------------------"<<endl;
+        cout << c;
+        //cout <<"Nombre del Equipo: "<<arreglo[i].getNombre()<<endl;
+        //cout <<"Sistema Operativo: "<<arreglo[i].getSistemaop()<<endl;
+        //cout <<"Marca del Equipo: "<<arreglo[i].getMarca()<<endl;
+        //cout <<"Memoria Ram: "<<arreglo[i].getMemoriaRam()<<"gb"<<endl;
+        //cout<<"-------------------------"<<endl;
     }
 }
