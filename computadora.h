99457af6b4fd6041c22e78b28becf8ea37a71d49
MiguelGuz <jose.guzman1343@alumnos.udg.mx>
2020-10-19 15:26:18 -0500

    agregacion de istream y ostream

--- computadora.h
+++ computadora.h
@@ -2,7 +2,7 @@
 #define COMPUTADORA_H
 
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 class computadora
@@ -25,6 +25,32 @@ public:
     string getMarca();
     void setMemoriaRam(float v);
     float getMemoriaRam();
+
+    friend ostream& operator<<(ostream &out,const computadora &c)
+    {
+        out << left;
+        out <<setw(13)<<c.nombre;
+        out <<setw(12)<<c.sistemaop;
+        out <<setw(10)<<c.marca;
+        out <<setw(10)<<c.memoriaRam;
+        out <<endl;
+         return out;
+    }
+
+    friend istream& operator>>(istream &in,computadora &c)
+    {
+        string temp;
+        float MemoriaRam;
+        cout<<"Nombre de cuenta: "<<endl;
+        getline(cin,c.nombre);
+        cout<<"sistema Operativo: "<<endl;
+        getline(cin,c.sistemaop);
+        cout<<"Pc Marca: "<<endl;
+        getline(cin,c.marca);
+        cout<<"Memoria Ram: "<<endl;
+        cin>>c.memoriaRam;
+        return in;
+    } 
 
 };
 
