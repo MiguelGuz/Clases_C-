ec4b840c0c2c17c6f37e016fa04c30a249340879
MiguelGuz <jose.guzman1343@alumnos.udg.mx>
2020-10-21 19:43:11 -0500

    txt

--- main.cpp
+++ main.cpp
@@ -6,29 +6,33 @@ using namespace std;
 
 int main(){
 
-    computadora c02 = computadora(  "migse",
-                                    "windows",
-                                    "lenovo",
-                                    4.00);
-    computadora c03;
-    c03.setNombre("usuario");
-    c03.setSistemaop("windows");
-    c03.setMarca("microsoft");
-    c03.setMemoriaRam(8.00);
-
     laboratorio lb;
+    lb.recuperar();
+    lb.mostrar();
+
+//computadora c02 = computadora(  "migse",
+//                                "windows",
+//                                "lenovo",
+//                                    4.00);
+//    computadora c03;
+//    c03.setNombre("usuario");
+//   c03.setSistemaop("windows");
+//    c03.setMarca("microsoft");
+//    c03.setMemoriaRam(8.00);
+
+//    laboratorio lb;
 
     //lb.agregarcomputadora(c02);
     //lb.agregarcomputadora(c03);
 
-    lb<<c02<<c03;
+    //lb<<c02<<c03;
 
     //cout<<c02;
-    computadora c04;
-    cin >> c04;
-    lb << c04;
+    //computadora c04;
+    //cin >> c04;
+    //lb << c04;
 
-    lb.mostrar();
+    //lb.mostrar();
 
     //cout<< c03.getNombre()<<endl;
     //cout<< c03.getSistemaop()<<endl;
